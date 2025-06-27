106df213a368ef04a7a39ffb235631a44ac3d37e
ej21cloud <mariclin00@naver.com>
2025-06-27 17:21:07 +0900

    회원탈퇴 기능

---
+++ backend/src/main/java/pack/dto/member/WithdrawRequest.java
@@ -0,0 +1,12 @@
+package pack.dto.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WithdrawRequest {
+    private String id;
+    private String password;
+}
+
