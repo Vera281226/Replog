c05e961070aad1f5d18cdca046f1b08de8267c41
acorn <acorn@BOOK-AGO4PO8PNG>
2025-07-10 10:40:08 +0900

    [추가] 아이디, 닉네임, 이메일 중복 확인 및 이메일 인증

--- backend/src/main/java/pack/service/member/MemberService.java
+++ backend/src/main/java/pack/service/member/MemberService.java
@@ -7,10 +7,10 @@ import pack.dto.member.UserInfoResponse;
 public interface MemberService {
 	boolean emailExists(String email); // 이메일 중복 확인
 	boolean nicknameExists(String nickname); // 닉네임 중복 확인
+	boolean idExists(String id);
     void signUp(SignUpRequest request); //회원가입 처리
     boolean validateLogin(String memberId, String password); // 로그인 검증
     boolean withdraw(String memberId, String password); // 탈퇴 처리
-    //void updatePassword(String email, String newPassword); // 비밀번호 재설정용 메서드
 	UserInfoResponse authenticateUser(String memberId, String password);
 	UserInfoResponse getUserInfo(String memberId);
 	MemberInfoResponse getMemberInfo(String memberId);
